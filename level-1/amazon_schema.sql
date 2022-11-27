4801a0a31d3d1bfdc68ea3e628f55455041977b1
Zach Wood <zachrwood28@gmail.com>
2022-11-27 16:19:25 -0600

    Updates to Level 1 notebook

---
+++ level-1/amazon_schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE review_id_table (
+  review_id TEXT PRIMARY KEY NOT NULL,
+  customer_id INTEGER,
+  product_id TEXT,
+  product_parent INTEGER,
+  review_date DATE -- this should be in the formate yyyy-mm-dd
+);
+
+-- This table will contain only unique values
+CREATE TABLE products (
+  product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+  product_title TEXT
+);
+
+-- Customer table for first data set
+CREATE TABLE customers (
+  customer_id INT PRIMARY KEY NOT NULL UNIQUE,
+  customer_count INT
+);
+
+select * from review_id_table;
+
+select * from products;
+
+select * from customers;
