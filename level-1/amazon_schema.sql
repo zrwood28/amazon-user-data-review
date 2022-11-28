6e87010d128e632cccdb1a1c3283187bb37717e6
Zach Wood <zachrwood28@gmail.com>
2022-11-27 19:42:31 -0600

    Created DataFrames and populated RDS database.

--- level-1/amazon_schema.sql
+++ level-1/amazon_schema.sql
@@ -3,16 +3,14 @@ CREATE TABLE review_id_table (
   customer_id INTEGER,
   product_id TEXT,
   product_parent INTEGER,
-  review_date DATE -- this should be in the formate yyyy-mm-dd
+  review_date DATE
 );
 
--- This table will contain only unique values
 CREATE TABLE products (
   product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
   product_title TEXT
 );
 
--- Customer table for first data set
 CREATE TABLE customers (
   customer_id INT PRIMARY KEY NOT NULL UNIQUE,
   customer_count INT
